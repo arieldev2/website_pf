2aa2e06bbd2cfeaf786990f9bfd3c4c474bad4aa
ariel0123 <llamas0123@hotmail.com>
2021-09-22 16:36:43 -0700

    added observatory

---
+++ src/Data/projects.js
@@ -0,0 +1,60 @@
+export const projects = [
+    {
+        id: 0,
+        img: "/images/observatory.png",
+        title: "Observatory Presentation",
+        description: "Observatory Presentation was created with SwiftUI for macOS, you can create minimalist presentations by adding text, 3d models, images and videos.",
+        url: "https://apps.apple.com/us/app/observatory-presentation/id1585725474",
+        buttonText: "Go to App Store"
+    },
+    {
+        id: 1,
+        img: "/images/note_t.png",
+        title: "Note Thread",
+        description: "This note app was created with SwiftUI for iOS, you can create a note, edit note, delete note, check notes, select by categories and sort by checked and unchecked.",
+        url: "https://apps.apple.com/us/app/note-thread/id1583119687",
+        buttonText: "Go to App Store"
+    },
+    {
+        id: 2,
+        img: "/images/incomenify_w.png",
+        title: "Incomenify",
+        description: "App for Income management, made with React Native.",
+        url: "https://play.google.com/store/apps/details?id=com.WaveGamesPlay.Incomenify",
+        buttonText: "Go to Play Store"
+    },
+    {
+        id: 3,
+        img: "/images/note_w.png",
+        title: "Your Note",
+        description: "App for notes, made with React Native.",
+        url: "https://play.google.com/store/apps/details?id=com.WaveGamesPlay.YourNote",
+        buttonText: "Go to Play Store"
+    },
+    {
+        id: 4,
+        img: "/images/piggy_w.png",
+        title: "Piggy Jumper",
+        description: "Jump over obstacles to get the highest score, made with Unity.",
+        url: "https://play.google.com/store/apps/details?id=com.WaveGamesPlay.PiggyJumper",
+        buttonText: "Go to Play Store"
+    },
+    {
+        id: 5,
+        img: "/images/hollow_w.png",
+        title: "Hollow Pumpkin",
+        description: "Destroy all the Pumpkins before they catch you, made with Unity.",
+        url: "https://play.google.com/store/apps/details?id=com.WaveGamesPlay.HollowPumpkin",
+        buttonText: "Go to Play Store"
+    },
+    {
+        id: 6,
+        img: "/images/ufo_w.png",
+        title: "Crazy Ufo's",
+        description: "Destroy all the Ufo's before they destroy the planet, made with Unity.",
+        url: "https://play.google.com/store/apps/details?id=com.WaveGamesPlay.CRAZYUFOs",
+        buttonText: "Go to Play Store"
+    }
+]
+
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import {projects} from '../Data/projects'
+
 
 
 
 
 export default function Home() {
+
+
     return (
 
         <div className="mar-b">
@@ -29,69 +33,29 @@ export default function Home() {
 
                 <div className="row mt-3">
 
+                { projects.map(item =>  (
                 
-                    <div className="card col-md card-color m-2">
-                        <img className="card-img-top" src={process.env.PUBLIC_URL +"/images/note_w.png"} alt="..."/>
+                <div className="col-md-4 p-1" key={item.id}>
+                    <div className="card  card-color " >
+                        <img className="card-img-top" src={process.env.PUBLIC_URL +item.img} alt="..."/>
                         <div className="card-body">
-                            <h5 className="card-title text-white">Your Note</h5>
-                            <p className="text-white">App for notes, made with React Native.</p>
+                            <h5 className="card-title text-white">{item.title}</h5>
+                            <p className="text-white">{item.description}</p>
 
-                            <a href={"https://play.google.com/store/apps/details?id=com.WaveGamesPlay.YourNote"} className="btn btn-primary">Go to Play Store</a>
+                            <a href={item.url} className="btn btn-primary">{item.buttonText}</a>
                         </div>
                     </div>
-                    <div className="card col-md card-color m-2">
-                        <img className="card-img-top" src={process.env.PUBLIC_URL +"/images/incomenify_w.png"} alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title text-white">Incomenify</h5>
-                            <p className="text-white">App for Income management, made with React Native.</p>
-
-                            <a href={"https://play.google.com/store/apps/details?id=com.WaveGamesPlay.Incomenify"} className="btn btn-primary">Go to Play Store</a>
-                        </div>
                     </div>
+                    
 
-                    <div className="card col-md card-color m-2">
-                        <img className="card-img-top" src={process.env.PUBLIC_URL +"/images/note_t.png"} alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title text-white">Note Thread</h5>
-                            <p className="text-white">This note app was created with SwiftUI, you can create a note, edit note, delete note, check notes, select by categories and sort by checked and unchecked.</p>
-                            <a href={"https://apps.apple.com/us/app/note-thread/id1583119687"} className="btn btn-primary">Go to App Store</a>
-                        </div>
-                    </div>
+                    ))
+                   
+                }
 
                   
                 </div>
 
 
-
-                <div className="row ">
-                   
-                    <div className="card col-md card-color m-2">
-                        <img className="card-img-top" src={process.env.PUBLIC_URL +"/images/piggy_w.png"} alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title text-white">Piggy Jumper</h5>
-                            <p className="text-white">Jump over obstacles to get the highest score, made with Unity.</p>
-
-                            <a href={"https://play.google.com/store/apps/details?id=com.WaveGamesPlay.PiggyJumper"} className="btn btn-primary">Go to Play Store</a>
-                        </div>
-                    </div>
-                    <div className="card col-md card-color m-2">
-                        <img className="card-img-top" src={process.env.PUBLIC_URL +"/images/hollow_w.png"} alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title text-white">Hollow Pumpkin</h5>
-                            <p className="text-white">Destroy all the Pumpkins before they catch you, made with Unity.</p>
-                            <a href={"https://play.google.com/store/apps/details?id=com.WaveGamesPlay.HollowPumpkin"} className="btn btn-primary">Go to Play Store</a>
-                        </div>
-                    </div>
-
-                    <div className="card col-md card-color m-2">
-                        <img className="card-img-top" src={process.env.PUBLIC_URL +"/images/ufo_w.png"} alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title text-white">Crazy Ufo's</h5>
-                            <p className="text-white">Destroy all the Ufo's before they destroy the planet, made with Unity.</p>
-                            <a href={"https://play.google.com/store/apps/details?id=com.WaveGamesPlay.CRAZYUFOs"} className="btn btn-primary">Go to Play Store</a>
-                        </div>
-                    </div>
-                </div>
 
                     
                     
