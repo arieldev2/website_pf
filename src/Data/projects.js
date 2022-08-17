b6107461c670a56a705488c8146c166e6ed9f629
Ariel Ortiz <arielortiz.dev@gmail.com>
2022-08-17 14:48:41 -0700

    add ps

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,6 +1,14 @@
 export const projects = [
     {
         id: 0,
+        img: "/images/ps.png",
+        title: "Post Screenshot",
+        description: "Post Screenshot was created with SwiftUI, you can add a background to your screenshots to make them look better.",
+        url: "https://apps.apple.com/mx/app/post-screenshot/id1640179261",
+        buttonText: "Go to App Store"
+    },
+    {
+        id: 1,
         img: "/images/softlight.png",
         title: "Softlight Pro",
         description: "Softlight Pro was created with SwiftUI, is a photo editor that allows you to make adjustments to your photos.",
@@ -8,7 +16,7 @@ export const projects = [
         buttonText: "Go to App Store"
     },
     {
-        id: 1,
+        id: 2,
         img: "/images/converter.png",
         title: "Reality OBJ - USDZ Converter",
         description: "Reality OBJ - USDZ Converter was created with Swift UIKit, you can convert obj files to usdz and update materials.",
@@ -16,7 +24,7 @@ export const projects = [
         buttonText: "Go to App Store"
     },
     {
-        id: 2,
+        id: 3,
         img: "/images/usdz.png",
         title: "USDZ 3D Viewer",
         description: "USDZ 3D Viewer was created with SwiftUI, you can view your 3D USDZ models, change the background and change the lighting.",
@@ -25,7 +33,7 @@ export const projects = [
     },
   
     {
-        id: 3,
+        id: 4,
         img: "/images/observatory.png",
         title: "Observatory Presentation",
         description: "Observatory Presentation was created with SwiftUI for macOS, you can create minimalist presentations by adding text, 3d models, images and videos.",
@@ -33,7 +41,7 @@ export const projects = [
         buttonText: "Go to App Store"
     },
     {
-        id: 4,
+        id: 5,
         img: "/images/nt.png",
         title: "Note Thread",
         description: "This note app was created with SwiftUI for iOS, you can create a note, edit note, delete note, check notes, select by categories and sort by checked and unchecked.",
