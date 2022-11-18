70c5ef9667b95c3f42e2ff243922cccbb7773144
Ariel Ortiz <arielortiz.llamas@gmail.com>
2022-11-17 21:36:35 -0800

    Updates

---
+++ precache-manifest.115315bf490a1d59e0db52c86e33e33b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0779a7dc47dfb66d53ea",
+    "url": "/static/css/main.17b7a236.chunk.css"
+  },
+  {
+    "revision": "0779a7dc47dfb66d53ea",
+    "url": "/static/js/main.0779a7dc.chunk.js"
+  },
+  {
+    "revision": "d78e6fe8b29ad8d37c4b",
+    "url": "/static/css/1.8f35c354.chunk.css"
+  },
+  {
+    "revision": "d78e6fe8b29ad8d37c4b",
+    "url": "/static/js/1.d78e6fe8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "64dc66ff54984aa7d58d798bd133293d",
+    "url": "/index.html"
+  }
+];
