9aac3407ef142aef83786fdd80ba44a9df646bb8
Ariel Ortiz <arielortiz.dev@gmail.com>
2023-08-02 21:05:51 -0700

    add gh

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -27,7 +27,7 @@ export const projects = [
         id: 3,
         img: "/images/usdz.png",
         title: "USDZ 3D Viewer",
-        description: "USDZ 3D Viewer was created with SwiftUI, you can view your 3D USDZ models, change the background and change the lighting.",
+        description: "USDZ 3D Viewer was created with SwiftUI, you can view your 3D USDZ models, change the background and change the lighting with HDRI.",
         url: "https://apps.apple.com/us/app/usdz-3d-viewer/id1587642013#?platform=iphone",
         buttonText: "Go to App Store"
     },
@@ -52,29 +52,3 @@ export const projects = [
 ]
 
 
-export const games = [
-    {
-        id: 0,
-        img: "/images/piggy_w.png",
-        title: "Piggy Jumper",
-        description: "Jump over obstacles to get the highest score, made with Unity.",
-        url: "https://play.google.com/store/apps/details?id=com.WaveGamesPlay.PiggyJumper",
-        buttonText: "Go to Play Store"
-    },
-    {
-        id: 1,
-        img: "/images/hollow_w.png",
-        title: "Hollow Pumpkin",
-        description: "Destroy all the Pumpkins before they catch you, made with Unity.",
-        url: "https://play.google.com/store/apps/details?id=com.WaveGamesPlay.HollowPumpkin",
-        buttonText: "Go to Play Store"
-    },
-    {
-        id: 2,
-        img: "/images/ufo_w.png",
-        title: "Crazy Ufo's",
-        description: "Destroy all the Ufo's before they destroy the planet, made with Unity.",
-        url: "https://play.google.com/store/apps/details?id=com.WaveGamesPlay.CRAZYUFOs",
-        buttonText: "Go to Play Store"
-    }
-]
--- src/components/Home.js
+++ src/components/Home.js
@@ -18,11 +18,11 @@ export default function Home() {
                 <div className="row">
                 <div className="col-md ">
           <h1 className="mb-3 display-3">iOS developer</h1>
-                <p className="text-about">Hello, my name is Ariel Ortiz and I’m a iOS developer. I studied engineering in multimedia, I have experience creating mobile apps with Swift(iOS) and I also make video games with Unity. I’m always learning new things to expand my knowledge.</p>
+                <p className="text-about">Hello, my name is Ariel Ortiz and I’m a iOS developer. I studied engineering in multimedia, I have experience creating mobile apps with Swift and Objective-C. I’m always learning new things to expand my knowledge.</p>
           </div>
   
           <div className="col-md text-center mt-4">
-                <a href="https://github.com/Ariel0123" className="align fs-4"><FaGithub size={35} className="p-1"/> GitHub</a>
+                <a href="https://github.com/arieldev2" className="align fs-4"><FaGithub size={35} className="p-1"/> GitHub</a>
                 <a href="https://www.linkedin.com/in/ariel-ortiz-b66979186/" className="align fs-4"><FaLinkedin size={35} className="p-1"/> Linkedin</a>
             </div>
                 </div>
@@ -61,39 +61,6 @@ export default function Home() {
                     
             </div>
 
-
-            <div className="container mt-top2">
-                <h1 className="text-white sub sub-s">Video games</h1>
-
-                <div className="row mt-3">
-
-                { games.map(item =>  (
-                
-                <div className="col-md-4 p-1" key={item.id}>
-                    <div className="card  card-color " >
-                        <img className="card-img-top" src={process.env.PUBLIC_URL +item.img} alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title text-white">{item.title}</h5>
-                            <p className="text-white">{item.description}</p>
-
-                            <a href={item.url} className="btn btn-primary">{item.buttonText}</a>
-                        </div>
-                    </div>
-                    </div>
-                    
-
-                    ))
-                   
-                }
-
-                  
-                </div>
-
-
-
-                    
-                    
-            </div>
 
 
 
