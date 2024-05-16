ab6e78c9aca72dc5a82b578abfe1379e3c23af46
Ariel Llamas <arielortiz.dev@gmail.com>
2024-05-15 19:04:07 -0700

    sikcam added

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,6 +1,14 @@
 export const projects = [
     {
         id: 0,
+        img: "/images/sikcam.png",
+        title: "Sik Cam",
+        description: "Sik Cam was created with SwiftUI for iOS, A camera app to take photos without having to edit, since it has ready-to-use filters.",
+        url: "https://apps.apple.com/mx/app/sik-cam/id6502562209",
+        buttonText: "Go to App Store"
+    },
+    {
+        id: 1,
         img: "/images/ps.png",
         title: "Post Screenshot",
         description: "Post Screenshot was created with SwiftUI, you can add a background to your screenshots to make them look better.",
@@ -8,7 +16,7 @@ export const projects = [
         buttonText: "Go to App Store"
     },
     {
-        id: 1,
+        id: 2,
         img: "/images/softlight.png",
         title: "Softlight Pro",
         description: "Softlight Pro was created with SwiftUI, is a photo editor that allows you to make adjustments to your photos.",
@@ -16,7 +24,7 @@ export const projects = [
         buttonText: "Go to App Store"
     },
     {
-        id: 2,
+        id: 3,
         img: "/images/converter.png",
         title: "Reality OBJ - USDZ Converter",
         description: "Reality OBJ - USDZ Converter was created with Swift UIKit, you can convert obj files to usdz and update materials.",
@@ -24,7 +32,7 @@ export const projects = [
         buttonText: "Go to App Store"
     },
     {
-        id: 3,
+        id: 4,
         img: "/images/usdz.png",
         title: "USDZ 3D Viewer",
         description: "USDZ 3D Viewer was created with SwiftUI, you can view your 3D USDZ models, change the background and change the lighting with HDRI.",
@@ -33,21 +41,14 @@ export const projects = [
     },
   
     {
-        id: 4,
+        id: 5,
         img: "/images/observatory.png",
         title: "Observatory Presentation",
         description: "Observatory Presentation was created with SwiftUI for macOS, you can create minimalist presentations by adding text, 3d models, images and videos.",
         url: "https://apps.apple.com/us/app/observatory-presentation/id1585725474",
         buttonText: "Go to App Store"
     },
-    {
-        id: 5,
-        img: "/images/nt.png",
-        title: "Note Thread",
-        description: "This note app was created with SwiftUI for iOS, you can create a note, edit note, delete note, check notes, select by categories and sort by checked and unchecked.",
-        url: "https://apps.apple.com/us/app/note-thread/id1583119687",
-        buttonText: "Go to App Store"
-    },
+   
     
 ]
 
