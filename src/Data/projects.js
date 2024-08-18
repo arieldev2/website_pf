bb198cbeb81ecaac44ce3187a649715e44622680
Ariel Llamas <arielortiz.dev@gmail.com>
2024-08-17 19:16:56 -0700

    app removed

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -4,43 +4,27 @@ export const projects = [
         img: "/images/sikcam.png",
         title: "Sik Cam",
         description: "Sik Cam was created with SwiftUI for iOS, A camera app to take photos without having to edit, since it has ready-to-use filters.",
-        url: "https://apps.apple.com/mx/app/sik-cam/id6502562209",
+        url: "https://apps.apple.com/us/app/sik-cam/id6502562209",
         buttonText: "Go to App Store"
     },
+
     {
         id: 1,
-        img: "/images/ps.png",
-        title: "Post Screenshot",
-        description: "Post Screenshot was created with SwiftUI, you can add a background to your screenshots to make them look better.",
-        url: "https://apps.apple.com/mx/app/post-screenshot/id1640179261",
+        img: "/images/converter.png",
+        title: "Reality OBJ - USDZ Converter",
+        description: "Reality OBJ - USDZ Converter was created with Swift UIKit, you can convert obj files to usdz and update materials.",
+        url: "https://apps.apple.com/us/app/reality-obj-usdz-converter/id1603967912",
         buttonText: "Go to App Store"
     },
     {
         id: 2,
-        img: "/images/converter.png",
-        title: "Reality OBJ - USDZ Converter",
-        description: "Reality OBJ - USDZ Converter was created with Swift UIKit, you can convert obj files to usdz and update materials.",
-        url: "https://apps.apple.com/mx/app/reality-obj-usdz-converter/id1603967912",
-        buttonText: "Go to App Store"
-    },
-    {
-        id: 3,
         img: "/images/usdz.png",
         title: "USDZ 3D Viewer",
-        description: "USDZ 3D Viewer was created with SwiftUI, you can view your 3D USDZ models, change the background and change the lighting with HDRI.",
+        description: "USDZ 3D Viewer was created with SwiftUI, you can view your 3D USDZ models, change the background and lighting with HDRI.",
         url: "https://apps.apple.com/us/app/usdz-3d-viewer/id1587642013#?platform=iphone",
         buttonText: "Go to App Store"
     },
-  
-    {
-        id: 4,
-        img: "/images/observatory.png",
-        title: "Observatory Presentation",
-        description: "Observatory Presentation was created with SwiftUI for macOS, you can create minimalist presentations by adding text, 3d models, images and videos.",
-        url: "https://apps.apple.com/us/app/observatory-presentation/id1585725474",
-        buttonText: "Go to App Store"
-    },
-   
+     
     
 ]
 
