e0d76b79d7e3ccb4778dd3db692b2c7784b09aed
Ariel Ortiz <arielortiz.dev@gmail.com>
2024-09-06 00:00:43 -0700

    sv cam

--- src/components/appOne/AppOne.js
+++ src/components/appOne/AppOne.js
@@ -11,15 +11,15 @@ function AppOne() {
         />
       </div>
       <div class="item">
-        <h1 className="app1_title">Sik Cam</h1>
+        <h1 className="app1_title">SV Cam</h1>
       </div>
       <div class="item">
         <p className="content">
-          Sik Cam was created with SwiftUI. A camera app to take photos without
+          SV Cam was created with SwiftUI. A camera app to take photos without
           having to edit, since it has ready-to-use filters.
         </p>
         <a
-          href="https://apps.apple.com/us/app/sik-cam/id6502562209"
+          href="https://apps.apple.com/us/app/sv-cam-vintage-camera/id6502562209"
           style={{ marginTop: "15px" }}
         >
           <img
